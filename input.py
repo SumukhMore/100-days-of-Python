5c3c2b1a829fa5e801b70a7ff84d4dfcafa14b80
SumukhMore <sumukhmore@gmail.com>
2026-01-11 20:16:54 +0530

    Input-Code

---
+++ input.py
@@ -0,0 +1,5 @@
+#Taking input from user & printing it.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+price = float(input("Enter the price of the item: "))
+print("Hello, my name is", name, "and I am", age, "years old.")
