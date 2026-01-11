7a01b3838a7cbaa6839eafd61a530e9dfd667f60
SumukhMore <sumukhmore@gmail.com>
2026-01-11 17:41:20 +0530

    Variables

---
+++ Variables.py
@@ -0,0 +1,16 @@
+name = "Sumukh More" #string variable
+age = 20 #integer variable
+age2 = age + 5  # Adding 5 to age
+price = 49.99 #float variable
+is_student = True #boolean variable
+
+print("My name is: ", name)
+print("My age is: ", age)
+print("My age after 5 years will be: ", age2)
+print("The price is: $", price)
+print("I am a student: ", is_student)
+
+print(type(name)) #checking the data type of variables
+print(type(age))
+print(type(price))
+print(type(is_student))
