16d751b9083bee209a92c1641ae9cfa53f3d5ba9
SumukhMore <sumukhmore@gmail.com>
2026-01-12 14:42:28 +0530

    Conditional-Statements

---
+++ conditional-statements.py
@@ -0,0 +1,10 @@
+light = input("Color of the traffic light : ")
+
+if (light == "Red"):
+    print("Stop")
+elif (light == "Yellow"):
+    print("Get Ready")
+elif (light == "Green"):
+    print("Go")
+else:
+    print("Invalid Color")    
