7bce571515a23277eb988675e5926830dcd1b2f7
SumukhMore <sumukhmore@gmail.com>
2026-01-12 16:05:22 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,58 @@
+#arithmatic operators
+a = 5
+b = 2
+
+print(a+b)    #Addition
+print(a-b)    #Subtraction
+print(a*b)    #Multiplication
+print(a/b)    #Division
+print(a//b)   #Floor Division
+print(a%b)    #Modulus
+print(a**b)   #Exponentiation
+
+#relational operators
+a = 50
+b = 20
+
+print(a > b)   #Greater than
+print(a < b)   #Less than
+print(a >= b)  #Greater than or equal to
+print(a <= b)  #Less than or equal to
+print(a == b)  #Equal to
+print(a != b)  #Not equal to
+
+#assignment operators
+num = 10
+num += 5   #num = num + 5
+print(num)
+
+num = 10
+num -= 5   #num = num - 5
+print(num)
+
+num = 10
+num *= 5   #num = num * 5
+print(num)
+
+num = 10
+num /= 5   #num = num / 5
+print(num)
+
+num = 10
+num %= 5   #num = num % 5
+print(num)
+
+num = 10
+num //= 5  #num = num // 5
+print(num)
+
+num = 10
+num **= 5  #num = num ** 5
+print(num)
+
+#logical operators
+x = 10
+y = 20
+print(x > 5 and y > 15)  #Logical AND
+print(x > 5 or y < 15)   #Logical OR
+print(not(x > 5))        #Logical NOT
